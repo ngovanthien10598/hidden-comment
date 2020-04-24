ec2a65fa944902c88699625e6e3d1320d8340348
Ngo Van Thien <thienyl6716@gmail.com>
2020-04-24 17:35:31 +0700

    Remove build from gitignore

---
+++ build/precache-manifest.b74e4af04f4a3b0e55ec4eb0758bc1b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc0907fbdb5ef36a398fe47916017486",
+    "url": "/hidden-comment/index.html"
+  },
+  {
+    "revision": "645678cae9f7057c1ffb",
+    "url": "/hidden-comment/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "42c1fc89f59aa37c4aa4",
+    "url": "/hidden-comment/static/js/2.86c8698d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hidden-comment/static/js/2.86c8698d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "645678cae9f7057c1ffb",
+    "url": "/hidden-comment/static/js/main.582a9776.chunk.js"
+  },
+  {
+    "revision": "45d05c960015ee78dc29",
+    "url": "/hidden-comment/static/js/runtime-main.4a1386f6.js"
+  }
+]);
